a710079e397c6f05b6aaeae53f4467185b488e72
tomas olivari <tomas_olivari96@hotmail.com>
2023-12-21 18:52:22 -0300

    fourth commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,20 +2,20 @@ import { CartWidget } from "./CartWidget";
 
 function Navbar() {
   return (
-    <nav className="flex justify-between">
-      <a className="p-4 text-xl hover:text-white " href="/">Logo</a>
+    <nav className="flex justify-between items-center text-gray-700 bg-white h-20">
+      <a className="p-4 text-xl" href="/">E-Commerce</a>
       <ul className="flex mr-4">
-        <li className="p-4  hover:text-white">
+        <li className="p-4 hover:text-red-400">
           <a href="/">Inicio</a>
         </li>
-        <li className="p-4 hover:text-white">
+        <li className="p-4 hover:text-red-400">
           <a href="/productos">Productos</a>
         </li>
-        <li className="p-4 hover:text-white">
+        <li className="p-4 hover:text-red-400">
           <a href="/contacto">Contacto</a>
         </li>
         
-        <li className="p-4 ml-10 text-white cursor-pointer">
+        <li className="p-4 ml-10 hover:text-red-400 cursor-pointer">
           <CartWidget />
         </li>
       </ul>
